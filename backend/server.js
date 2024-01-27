2c4061496ae2eb5eb646a370c8555bf28d05157d
sreedhar111yu <sreedharv36@gmail.com>
2024-01-27 14:13:23 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+
+console.log('MongoDB URI:', process.env.MONGODB_URI); // Log the MongoDB URI
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+const app = express();
+
+
+
+app.listen(4000, () => {
+    console.log("Server running on port 4000!");
+});
